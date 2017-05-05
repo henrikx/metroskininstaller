9b081e3849853f00a3d5daad55d5de09fc9e634f
henrikx <00heols@gmail.com>
2017-05-05 07:01:43 +0200

    Import from VS

---
+++ Metro Skin Installer/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Metro_Skin_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.noPatch = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.withPatch = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.downloadFileWorker = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.noPatch.SuspendLayout();
+            this.withPatch.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Metro for Steam";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(33, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(408, 171);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Welcome to this unofficial installer of Metro For Steam (with unofficial patch)!\n" +
+    "Select if you want the community patched version or the official version below a" +
+    "nd press next!\n";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Metro For Steam";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(165, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Metro For Steam (With Patch)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Location = new System.Drawing.Point(18, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 65);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(15, 227);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(453, 180);
+            this.panel2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // noPatch
+            // 
+            this.noPatch.Controls.Add(this.label2);
+            this.noPatch.Controls.Add(this.button4);
+            this.noPatch.Controls.Add(this.button2);
+            this.noPatch.Location = new System.Drawing.Point(492, 41);
+            this.noPatch.Name = "noPatch";
+            this.noPatch.Size = new System.Drawing.Size(453, 180);
+            this.noPatch.TabIndex = 8;
+            this.noPatch.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Press next to start the installation";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(375, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Next";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // withPatch
+            // 
+            this.withPatch.Controls.Add(this.checkedListBox1);
+            this.withPatch.Controls.Add(this.label4);
+            this.withPatch.Controls.Add(this.label3);
+            this.withPatch.Controls.Add(this.button5);
+            this.withPatch.Controls.Add(this.button6);
+            this.withPatch.Location = new System.Drawing.Point(492, 253);
+            this.withPatch.Name = "withPatch";
+            this.withPatch.Size = new System.Drawing.Size(453, 180);
+            this.withPatch.TabIndex = 9;
+            this.withPatch.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Press next to start the installation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select optional extras:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(375, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Next";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(294, 154);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Previous";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(21, 20);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // downloadFileWorker
+            // 
+            this.downloadFileWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.downloadFile_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1307, 701);
+            this.Controls.Add(this.withPatch);
+            this.Controls.Add(this.noPatch);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Installer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.noPatch.ResumeLayout(false);
+            this.noPatch.PerformLayout();
+            this.withPatch.ResumeLayout(false);
+            this.withPatch.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel noPatch;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel withPatch;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.ComponentModel.BackgroundWorker downloadFileWorker;
+    }
+}
+
---
+++ Metro Skin Installer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.Net;
+using System.IO;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+
+namespace Metro_Skin_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                noPatch.Visible = true;
+            }
+            else if (radioButton2.Checked)
+            {
+                withPatch.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Please select a version!");
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            noPatch.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            withPatch.Visible = false;
+        }
+
+        public string getSteamSkinPath()
+        {
+            using (var registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Valve\\Steam"))
+            {
+                string filePath = null;
+                var regFilePath = registryKey?.GetValue("SteamPath");
+                if (regFilePath != null)
+                {
+                    filePath = Path.Combine(regFilePath.ToString().Replace(@"/",@"\"), "skins");
+                }
+                return filePath;
+            }
+
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string steamSkinPath = getSteamSkinPath();
+            if(Directory.Exists (steamSkinPath))
+            {
+                richTextBox1.AppendText("\nSteam skin directory found: "+ steamSkinPath);
+                WebClient downloadFile = new WebClient();
+                richTextBox1.AppendText("\nLooking for latest version of Metro For Steam");
+                string source = Convert.ToString(downloadFile.DownloadString("http://metroforsteam.com"));
+                var regex = Regex.Match(source, "href=\"downloads(\\/*.*.zip)\"");
+                if (Convert.ToString(regex) != "")
+                {
+                    string downloadUrl = "http://metroforsteam.com/downloads" + Convert.ToString(regex.Groups[1].Value);
+                    richTextBox1.AppendText("\nFound latest version: " + Convert.ToString(downloadUrl));
+                    downloadFileWorker.RunWorkerAsync();
+                } else
+                {
+                    richTextBox1.AppendText("\nCouldn't find latest version on metroforsteam.com");
+                }
+
+            } else
+            {
+                richTextBox1.AppendText("\nCould not find a Steam installation");
+            }
+
+        }
+
+        private void downloadFile_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+    }
+}
