21e6e69ed20e43a036d3c9cf16f9238b851a1ee6
Henrik Takset <00heols@gmail.com>
2019-09-28 15:00:47 +0200

    update version number

--- Metro Skin Installer/Metro Skin Installer/Properties/AssemblyInfo.cs
+++ Metro Skin Installer/Metro Skin Installer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.9")]
-[assembly: AssemblyFileVersion("1.4.9")]
+[assembly: AssemblyVersion("1.4.10")]
+[assembly: AssemblyFileVersion("1.4.10")]
