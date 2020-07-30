28669bb7a2e44624a8e10651d9a3adca8a8777c4
Henrik <00heols@gmail.com>
2020-07-30 02:03:12 +0200

    prepare release

--- Metro Skin Installer/Metro Skin Installer/Properties/AssemblyInfo.cs
+++ Metro Skin Installer/Metro Skin Installer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.11")]
-[assembly: AssemblyFileVersion("1.4.11")]
+[assembly: AssemblyVersion("1.4.12")]
+[assembly: AssemblyFileVersion("1.4.12")]
