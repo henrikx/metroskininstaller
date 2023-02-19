21b573bc706c5bd42a6c842d99324dbc4328b72e
henrikx <00heols@gmail.com>
2023-02-19 20:34:51 +0100

    update build number

--- Metro Skin Installer/Metro Skin Installer/Properties/AssemblyInfo.cs
+++ Metro Skin Installer/Metro Skin Installer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.15")]
-[assembly: AssemblyFileVersion("1.4.15")]
+[assembly: AssemblyVersion("1.4.16")]
+[assembly: AssemblyFileVersion("1.4.16")]
